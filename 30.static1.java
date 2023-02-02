75acc481d939cd31a0a59bbc6f4ae9cd67f68adc
Payal Pansuriya <110537306+Payall13@users.noreply.github.com>
2023-02-02 16:55:06 +0530

    Create 30.static1.java

---
+++ 30.static1.java
@@ -0,0 +1,13 @@
+class static1
+{
+	static int x=100;
+	static void disp()
+	{
+		System.out.println("disp");
+	}
+	public static void main(String args[])
+	{
+		disp();
+		System.out.println("x="+x);
+	}
+}
