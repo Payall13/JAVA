481fc80fbfef6b249024910a640425199a8a594c
Payal Pansuriya <110537306+Payall13@users.noreply.github.com>
2023-02-08 17:51:30 +0530

    Create 37.consinheritance1.java

---
+++ 37.consinheritance1.java
@@ -0,0 +1,21 @@
+class A
+{
+	A()
+	{
+		System.out.println("class A");
+	}
+}
+class B extends A
+{
+	B()
+	{
+		System.out.println("class B");
+	}
+}
+class consinheritance1
+{
+	public static void main(String args[])
+	{
+		B b1=new B();
+	}
+}
