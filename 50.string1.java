95eded6f69d83a3bd4248cafac62e88ec0e32bc4
Payal Pansuriya <110537306+Payall13@users.noreply.github.com>
2023-02-13 16:49:12 +0530

    Create 50.string1.java

---
+++ 50.string1.java
@@ -0,0 +1,15 @@
+class string1
+{
+	public static void main(String args[])
+	{
+		String s1="String using literal";
+		String s2=new String ("string using new");
+		char c1[]={'a','b','c','d','e'};
+		String s3=new String (c1);
+		System.out.println("s1="+s1);
+		System.out.println("s2="+s2);
+		System.out.println("s3="+s3);
+	}
+}
+		
+		
