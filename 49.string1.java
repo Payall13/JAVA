ace5e1b3602e9a5f8db731472920a7346287e0f7
Payal Pansuriya <110537306+Payall13@users.noreply.github.com>
2023-02-13 16:44:05 +0530

    Create 49.string1.java

---
+++ 49.string1.java
@@ -0,0 +1,12 @@
+class string1
+{
+	public static void main(String args[])
+	{
+		String s1="String using literal";
+		String s2=new String ("string using new");
+		System.out.println("s1="+s1);
+		System.out.println("s2="+s2);
+	}
+}
+		
+		
