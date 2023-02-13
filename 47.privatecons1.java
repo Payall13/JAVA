1ab4f086ec62378af64a3e1b6f744c84ae8eb87c
Payal Pansuriya <110537306+Payall13@users.noreply.github.com>
2023-02-13 16:38:22 +0530

    Create 47.privatecons1.java

---
+++ 47.privatecons1.java
@@ -0,0 +1,11 @@
+class privatecons1
+{
+	private privatecons1()
+	{
+		System.out.println("private constructor");
+	}
+	public static void main(String args[])
+	{
+		privatecons1 pc1=new privatecons1();
+	}
+}
